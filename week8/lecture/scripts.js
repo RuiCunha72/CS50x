8865eb9dc8b42d9c9a3d3d98b92d08a76b8eee5d
RuiCunha72 <ruicunha72@gmail.com>
2025-04-29 16:48:37 +0100

    week8 lecture

---
+++ week8/lecture/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('form').addEventListener('submit', function(event) {
+        alert('Hello, ' + document.querySelector('#name').value);
+        event.preventDefault();
+    });
+});
